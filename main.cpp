e31c1b3e6d6642ede421f962df344912c5847776
RickardoMartinez <145060734+RickardoMartinez@users.noreply.github.com>
2023-09-28 20:07:47 +0000

    Initial commit

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Getting Started on the Unit 6 Assignment Dr. T
+#include <algorithm> 
 #include <iostream>
 #include "accounts.h"
 using namespace std;
@@ -6,6 +6,54 @@ using namespace std;
 
 int main() 
 {
+int temp = 0;
+  for (int left = 0; left < maxAccounts; left++)
+    {
+      for (int right = left+1; right < maxAccounts; right++)
+        {
+          if (accountBalances[left] > accountBalances[right])
+          {
+            temp = accountBalances[left];
+            accountBalances[left] = accountBalances[right];
+            accountBalances[right] = temp;
+          }
+        }
+    }
+    cout << "\nPrinted Values: "<< endl;
+  for(int i = 0; i < maxAccounts; i++)
+    {
+      cout << accountBalances[i]<<endl;
+    }
+  cout << "End" << cpuTime() << endl;
+cout << "Start: "<< cpuTime() << endl;
 
+sort(accountBalances,accountBalances+maxAccounts);
+  cout << "\nPrinted Values: " << endl;
+  
+  for(int i = 0; i < maxAccounts; i++)
+  {
+    cout << accountBalances[i] << endl; 
+  }
+cout << "End " << cpuTime() << endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
   return 0;
 }
